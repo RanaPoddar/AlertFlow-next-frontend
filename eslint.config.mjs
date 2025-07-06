6fcf93d9e824e1e2707ba8283012fbfa49ffd9f0
Rana Poddar <ranapoddarmath2@gmail.com>
2025-07-06 23:39:18 +0530

    eslint disabled

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,14 +11,34 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable all ESLint rules
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-img-element': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      'prefer-const': 'off',
+      'no-console': 'off',
+      'no-debugger': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'react/jsx-no-undef': 'off',
+      'react/prop-types': 'off',
+      'react/display-name': 'off',
+      'react/jsx-key': 'off',
+      'react/no-children-prop': 'off',
+      'react/jsx-no-target-blank': 'off',
+      'react/jsx-no-duplicate-props': 'off',
+      'react/no-deprecated': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-vars': 'off',
+      'react/jsx-fragments': 'off',
+    }
+  }
 ];
-
-// Disable 'no-unused-vars' errors and warnings globally
-eslintConfig.push({
-  rules: {
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-  },
-});
 
 export default eslintConfig;
